aa5f26209130710cc84b8cecfb6eb89aba38cdf8
Mauricio Goulart <mauriciogoulart23@live.com>
2021-02-26 15:54:10 -0300

    Repositorio referente a curso Java Web Full Stack JDev.
    -Primeiro Commit

---
+++ src/br/com/mauriciogoulart/modulo8/classes/Aluno.java
@@ -0,0 +1,21 @@
+package br.com.mauriciogoulart.modulo8.classes;
+
+public class Aluno {
+
+    //atributos
+
+    int idade;
+
+    String nome;
+    String dataNascimento;
+    String registroGeral;
+    String numeroCpf;
+    String nomeMae;
+    String nomePai;
+    String dataMatricula;
+    String nomeEscola;
+    String serieMatriculado;
+
+
+
+}
