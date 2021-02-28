a17a1c79c6da993c3920ffd47ae1088b9b3a50e1
Mauricio Goulart <mauriciogoulart23@live.com>
2021-02-28 00:14:55 -0300

    Constantes
    - Atributos estáticos

---
+++ src/br/com/mauriciogoulart/modulo8/constantes/StatusAluno.java
@@ -0,0 +1,15 @@
+package br.com.mauriciogoulart.modulo8.constantes;
+
+public class StatusAluno {
+
+    /*Atributos estaticos
+    * Obs: nome dos atributos estaticos sempre em caixa alta*/
+
+    public static String APROVADO = "Aprovado";
+    public static String RECUPERACAO = "Recuperacao";
+    public static String REPROVADO = "Reprovado";
+
+    /* Atributos e metodos estaticos podem ser acessados sem
+    a necessidade de instanciar o objeto*/
+
+}
