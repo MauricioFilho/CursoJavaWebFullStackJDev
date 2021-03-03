9547aa66a8e0b5a3ea5db0d582ab46c5d77ee592
Mauricio Goulart <mauriciogoulart23@live.com>
2021-03-03 16:17:10 -0300

    [Unidade 13 - Thread e programação paralela ou concorrente]
    [Thread]
    
    - Instanciando Thread com o Runnable

--- src/br/com/mauriciogoulart/threads/MainThread.java
+++ src/br/com/mauriciogoulart/threads/MainThread.java
@@ -7,25 +7,52 @@ public class MainThread {
     public static void main(String[] args) throws InterruptedException {
 
 
-        /*Declaração de uma thread para processamento paralelo*/
-        new Thread(){
-            @Override
-            public void run() { //run() executa o bloco de codigo dentro da tread
-                /*Bloco a ser executado em paralelo*/
-                for (int i = 0; i < 10; i++) {
+        /*Declaração de uma thread para processamento paralelo (envio de email)*/
+        Thread threadEmail = new Thread(thread1);//Objeto do tipo thread
+        threadEmail.start();
 
-                    /*executando tarefa com intervalo de tempo usando Theads*/
-                    System.out.println("Envio de email!!");
-                    try{
-                        Thread.sleep(1000); /*Para o tempo de execução em milisegundos*/
-                    }catch(Exception e){
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }.start(); //.start() executa a tread
+        /*Declaração de uma thread para processamento paralelo (envio de notas fiscais)*/
+        Thread threadNf = new Thread(thread2); //Objeto do tipo thread
+        threadNf.setPriority(Thread.MAX_PRIORITY); //Define a prioridade de execução entre as threads
+        threadNf.start();
 
         System.out.println("Processo chegou ao fim!");
         JOptionPane.showMessageDialog(null,"Sistema continua executando para o usuario!");
     }
+
+    /*Metodos Runnable para poder instanciar um objeto thread*/
+    private static Runnable thread1 = new Runnable() {
+        @Override
+        public void run() {
+
+            /*Bloco a ser executado em paralelo*/
+            for (int i = 0; i < 10; i++) {
+
+                /*executando tarefa com intervalo de tempo usando Theads*/
+                System.out.println("Envio de email!!");
+                try{
+                    Thread.sleep(1000); /*Para o tempo de execução em milisegundos*/
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    };
+
+    private static Runnable thread2 = new Runnable() {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+
+                /*executando tarefa com intervalo de tempo usando Theads*/
+                System.out.println("Envio de nota fiscal!!");
+                try{
+                    Thread.sleep(1000); /*Para o tempo de execução em milisegundos*/
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
 }
