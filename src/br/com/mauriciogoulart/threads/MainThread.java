7238245fe470c9335f5303b765222af86364531b
Mauricio Goulart <mauriciogoulart23@live.com>
2021-03-03 13:50:02 -0300

    [Unidade 13 - Thread e programação paralela ou concorrente]
    [Thread]
    
    - Primeira thread para execução paralela

--- src/br/com/mauriciogoulart/threads/MainThread.java
+++ src/br/com/mauriciogoulart/threads/MainThread.java
@@ -1,18 +1,31 @@
 package br.com.mauriciogoulart.threads;
 
+import javax.swing.*;
+
 public class MainThread {
 
     public static void main(String[] args) throws InterruptedException {
 
 
+        /*Declaração de uma thread para processamento paralelo*/
+        new Thread(){
+            @Override
+            public void run() { //run() executa o bloco de codigo dentro da tread
+                /*Bloco a ser executado em paralelo*/
+                for (int i = 0; i < 10; i++) {
 
-        for (int i = 0; i < 10; i++) {
+                    /*executando tarefa com intervalo de tempo usando Theads*/
+                    System.out.println("Envio de email!!");
+                    try{
+                        Thread.sleep(1000); /*Para o tempo de execução em milisegundos*/
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start(); //.start() executa a tread
 
-            /*executando tarefa com intervalo de tempo usando Theads*/
-
-            Thread.sleep(1000); /*Para o tempo de execução em milisegundos*/
-            System.out.println("Envio de email!!");
-
-        }
+        System.out.println("Processo chegou ao fim!");
+        JOptionPane.showMessageDialog(null,"Sistema continua executando para o usuario!");
     }
 }
